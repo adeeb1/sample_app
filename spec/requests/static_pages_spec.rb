c4282eb9af2d0cd9fe60ca09736ed042b32074b5
adeeb <adeeb1@babson.edu>
2013-05-17 17:34:02 -0400

    Added a subroutine to spec/requests/static_pages_spec.rb and replaced a lot of repeated code with it

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,60 +1,58 @@
 require 'spec_helper'
 
+def checkvalue(message, url, selector, text)
+  it "#{message}" do
+    # Visit the specified URL
+    visit url
+
+    # Concatenate the page's name to the end of the default
+    # "Ruby on Rails Tutorial Sample App" text if the
+    # selector is a 'title' tag
+    if (selector == 'title')
+      text = "Ruby on Rails Tutorial Sample App | " + text
+    end
+
+    # Check if the specified selector ('h1' or 'title')
+    # contains the value of 'text'
+    page.should have_selector(selector, :text => text)
+  end
+end
+
 describe "Static pages" do
 
   describe "Home page" do
+    
+    checkvalue("should have the content 'Sample App'",
+               '/static_pages/home', 'h1', 'Sample App')
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
-    end
-
-    it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-          :text => "Ruby on Rails Tutorial Sample App | Home")
-    end
+    checkvalue("should have the right title",
+               '/static_pages/home', 'title', 'Home')
   end
 
   describe "Help page" do
 
-	  it "should have the content 'Help'" do
-	    visit '/static_pages/help'
-	    page.should have_selector('h1', :text => 'Help')
-	  end
+    checkvalue("should have the content 'Help'",
+               '/static_pages/help', 'h1', 'Help')
 
-    it "should have the right title" do
-      visit '/static_pages/help'
-      page.should have_selector('title', 
-          :text => "Ruby on Rails Tutorial Sample App | Help")
-    end
+    checkvalue("should have the right title",
+               '/static_pages/help', 'title', 'Help')
   end
 
   describe "About page" do
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
-    end
+    checkvalue("should have the content 'About Us'",
+               '/static_pages/about', 'h1', 'About Us')
 
-    it "should have the right title" do
-      visit '/static_pages/about'
-      page.should have_selector('title',
-          :text => "Ruby on Rails Tutorial Sample App | About Us")
-    end
+    checkvalue("should have the right title",
+               '/static_pages/about', 'title', 'About Us')
   end
 
   describe "Contact page" do
     
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact')
-    end
+    checkvalue("should have the content 'Contact'",
+               '/static_pages/contact', 'h1', 'Contact')
 
-    it "should have the right title" do
-      visit '/static_pages/contact'
-      page.should have_selector('title',
-          :text => "Ruby on Rails Tutorial Sample App | Contact")
-    end
+    checkvalue("should have the right title",
+               '/static_pages/contact', 'title', 'Contact')
   end
 end
