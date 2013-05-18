58f142b7994cbe9c443ac495a31ae74cfe9f4092
adeeb <adeeb1@babson.edu>
2013-05-18 16:12:38 -0400

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+	# Returns the full title on a per-page basis
+	def full_title(page_title)
+		# Set the base title
+		base_title = "Ruby on Rails Tutorial Sample App"
+
+		# Check if a title was NOT specified on the page
+		if page_title.empty?
+			# It wasn't, so set the full title to the base
+			# title
+			return base_title
+		else # A title was specified on the page
+			# Concatenate the base title with the page's
+			# title
+			return "#{base_title} | #{page_title}"
+		end
+	end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,7 +9,7 @@ def checkvalue(message, url, selector, text)
     # "Ruby on Rails Tutorial Sample App" text if the
     # selector is a 'title' tag
     if (selector == 'title')
-      text = "Ruby on Rails Tutorial Sample App | " + text
+      text = "Ruby on Rails Tutorial Sample App" + text
     end
 
     # Check if the specified selector ('h1' or 'title')
@@ -18,41 +18,55 @@ def checkvalue(message, url, selector, text)
   end
 end
 
+def checknovalue(message, url, selector, text)
+  it "#{message}" do
+    # Visit the specified URL
+    visit url
+
+    # Check if the specified selector ('h1' or 'title')
+    # does NOT contain the value of 'text'
+    page.should_not have_selector(selector, :text => text)
+  end
+end
+
 describe "Static pages" do
 
   describe "Home page" do
     
-    checkvalue("should have the content 'Sample App'",
+    checkvalue("should have the h1 'Sample App'",
                '/static_pages/home', 'h1', 'Sample App')
 
-    checkvalue("should have the right title",
-               '/static_pages/home', 'title', 'Home')
+    checkvalue("should have the base title",
+               '/static_pages/home', 'title', '')
+
+    checknovalue("should not have a custom page title",
+                 '/static_pages/home', 'title', '| Home')
   end
 
   describe "Help page" do
 
-    checkvalue("should have the content 'Help'",
+    checkvalue("should have the h1 'Help'",
                '/static_pages/help', 'h1', 'Help')
 
-    checkvalue("should have the right title",
-               '/static_pages/help', 'title', 'Help')
+    checkvalue("should have the base title",
+               '/static_pages/help', 'title', ' | Help')
   end
 
   describe "About page" do
 
-    checkvalue("should have the content 'About Us'",
+    checkvalue("should have the h1 'About Us'",
                '/static_pages/about', 'h1', 'About Us')
 
-    checkvalue("should have the right title",
-               '/static_pages/about', 'title', 'About Us')
+    checkvalue("should have the base title",
+               '/static_pages/about', 'title', ' | About Us')
   end
 
   describe "Contact page" do
     
-    checkvalue("should have the content 'Contact'",
+    checkvalue("should have the h1 'Contact'",
                '/static_pages/contact', 'h1', 'Contact')
 
-    checkvalue("should have the right title",
-               '/static_pages/contact', 'title', 'Contact')
+    checkvalue("should have the base title",
+               '/static_pages/contact', 'title', ' | Contact')
   end
 end
