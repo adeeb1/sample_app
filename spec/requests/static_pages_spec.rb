117c4a49c8a1fa43dd74bdde8d58f8bae354d0ef
adeeb <adeeb1@babson.edu>
2013-05-19 14:52:56 -0400

    Finished website layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,72 +1,43 @@
 require 'spec_helper'
 
-def checkvalue(message, url, selector, text)
-  it "#{message}" do
-    # Visit the specified URL
-    visit url
-
-    # Concatenate the page's name to the end of the default
-    # "Ruby on Rails Tutorial Sample App" text if the
-    # selector is a 'title' tag
-    if (selector == 'title')
-      text = "Ruby on Rails Tutorial Sample App" + text
-    end
-
-    # Check if the specified selector ('h1' or 'title')
-    # contains the value of 'text'
-    page.should have_selector(selector, :text => text)
-  end
-end
-
-def checknovalue(message, url, selector, text)
-  it "#{message}" do
-    # Visit the specified URL
-    visit url
-
-    # Check if the specified selector ('h1' or 'title')
-    # does NOT contain the value of 'text'
-    page.should_not have_selector(selector, :text => text)
-  end
-end
-
 describe "Static pages" do
 
+  # Set the subject of all tests to the current webpage
+  subject { page }
+
   describe "Home page" do
-    
-    checkvalue("should have the h1 'Sample App'",
-               '/static_pages/home', 'h1', 'Sample App')
+    # Visit the root path before each example
+    before { visit root_path }
 
-    checkvalue("should have the base title",
-               '/static_pages/home', 'title', '')
-
-    checknovalue("should not have a custom page title",
-                 '/static_pages/home', 'title', '| Home')
+    it { should have_selector('h1',    text: 'Sample App') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
   end
 
   describe "Help page" do
+    # Visit the help path before each example
+    before { visit help_path }
 
-    checkvalue("should have the h1 'Help'",
-               '/static_pages/help', 'h1', 'Help')
-
-    checkvalue("should have the base title",
-               '/static_pages/help', 'title', ' | Help')
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title',
+         text: full_title('Help')) }
   end
 
   describe "About page" do
+    # Visit the about path before each example
+    before { visit about_path }
 
-    checkvalue("should have the h1 'About Us'",
-               '/static_pages/about', 'h1', 'About Us')
-
-    checkvalue("should have the base title",
-               '/static_pages/about', 'title', ' | About Us')
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title',
+         text: full_title('About Us')) }
   end
 
   describe "Contact page" do
-    
-    checkvalue("should have the h1 'Contact'",
-               '/static_pages/contact', 'h1', 'Contact')
+    # Visit the contact path before each example
+    before { visit contact_path }
 
-    checkvalue("should have the base title",
-               '/static_pages/contact', 'title', ' | Contact')
+    it { should have_selector('h1',    text: 'Contact') }
+    it { should have_selector('title',
+         text: full_title('Contact')) }
   end
 end
